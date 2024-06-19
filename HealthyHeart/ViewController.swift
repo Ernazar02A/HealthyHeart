c63bf6cfbaa7b09cfd083ed4503cb14caaf32a47
Ernazar <ernazaraibekov2017@gmail.com>
2024-06-19 17:20:12 +0600

    Initial Commit

---
+++ HealthyHeart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthyHeart
+//
+//  Created by Ernazar on 19/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
